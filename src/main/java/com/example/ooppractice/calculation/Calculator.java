d0c9e851544a8b70e76b89015c73060c3a45f0f3
rinikim <tytntu@gmail.com>
2022-10-18 19:43:02 +0900

    사칙 연산 계산기 만들기

---
+++ src/main/java/com/example/ooppractice/calculation/Calculator.java
@@ -0,0 +1,16 @@
+package com.example.ooppractice.calculation;
+
+public class Calculator {
+    public static int calculate(int operand1, String operator, int operand2) {
+        if ("+".equals(operator)) {
+            return operand1 + operand2;
+        } else if ("-".equals(operator)) {
+            return operand1 - operand2;
+        } else if ("*".equals(operator)) {
+            return operand1 * operand2;
+        } else if ("/".equals(operator)) {
+            return operand1 / operand2;
+        }
+        return 0;
+    }
+}
