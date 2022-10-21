c201d47d1581f9cb6f2e3473340cfcf53e9e1942
rinikim <tytntu@naver.com>
2022-10-21 20:05:22 +0900

    학점 계산기 만들기

---
+++ src/main/java/com/example/ooppractice/gradecalculate/GradeCalculator.java
@@ -0,0 +1,35 @@
+package com.example.ooppractice.gradecalculate;
+
+import java.util.List;
+
+public class GradeCalculator {
+
+    private List<Course> courses;
+
+    public GradeCalculator(List<Course> courses) {
+        this.courses = courses;
+    }
+
+    /**
+     * 요구사항
+     * 평균학점 계산 방법 = (학점수×교과목 평점)의 합계/수강신청 총학점 수
+     * MVC패턴(Model-View-Controller) 기반으로 구현한다.
+     * 일급 컬렉션 사용
+     */
+    public double calculateGrade() {
+        // (학점수×교과목 평점)의 합계
+        double multipliedCreditAndCourseGrade = 0;
+        for (Course course : courses) {
+            // 학점 * 성적을 숫자로 변환한 값 (예를들어 A+ = 95점)
+            multipliedCreditAndCourseGrade += course.getCredit() * course.getGradeToNumber();
+        }
+
+        // 수강신청 총학점 수
+        int totalCompletedCredit = courses.stream()
+                .mapToInt(Course::getCredit)
+                .sum();
+
+        return multipliedCreditAndCourseGrade / totalCompletedCredit;
+    }
+
+}
